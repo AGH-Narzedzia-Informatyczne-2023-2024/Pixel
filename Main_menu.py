46e809f61f221cf1a6e1631b6f048dd8f85f7e34
Maciej <147936508+mmaj1@users.noreply.github.com>
2023-11-26 01:38:02 +0100

    Update Main_menu.py

--- Main_menu.py
+++ Main_menu.py
@@ -1,5 +1,5 @@
 class Menu():
-    self.game = game
+    self,,,game = game
     self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
     self.run_display = True
     self.cursor_rect = pygame.React(0, 0, 20, 20)
